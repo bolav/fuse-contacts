80bfc713f770210eae9a687e05952164943dca43
Erik Faye-Lund <erik@outracks.com>
2016-09-22 13:45:05 +0200

    travis: always download latest fuse beta

--- .travis/install-fuse.sh
+++ .travis/install-fuse.sh
@@ -7,10 +7,10 @@
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 	# In /Users/travis/build/bolav/fuse-travis
-	echo "Installing Fuse version ${FUSE_VERSION}"
-	wget https://api.fusetools.com/fuse-release-management/releases/${FUSE_VERSION}/osx
-	mv osx fuse_osx_${FUSE_VERSION}.pkg
-	sudo installer -pkg fuse_osx_${FUSE_VERSION}.pkg -target /
-	echo "Installed Fuse ${FUSE_VERSION}"
+	echo "Installing latest Fuse beta version"
+	wget https://www.fusetools.com/downloads/latest/beta/osx -O fuse_osx.pkg
+	sudo installer -pkg fuse_osx.pkg -target /
+	echo "Installed Fuse"
+	fuse --version
 	mkdir -p $HOME/.fuse
 fi
